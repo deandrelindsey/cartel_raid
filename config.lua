4735df0c48b9e703f3a84bfb422f821f0049ca43
William Lindsey <deandrelindsey16@gmail.com>
2025-07-08 13:26:52 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,21 @@
-Config = {
-    DrugName = 'cartel_raid',
-    Enabled = true
+Config = {}
+
+Config.CartelZone = {
+    center = vector3(1385.0, 3600.0, 34.0), -- near Sandy Shores
+    radius = 60.0
 }
+
+Config.RaidCooldownHours = { min = 24, max = 84 }
+
+Config.GuardModels = {
+    `g_m_m_mexboss_01`,
+    `g_m_y_mexgoon_01`,
+    `g_m_y_mexgoon_02`
+}
+
+Config.CrateLoot = {
+    'cash_stack', 'pistol_ammo', 'cocaine_fishscale', 'weapon_pistol'
+}
+
+Config.AlertPolice = true
+Config.AllowedJobs = { "police" }
